9224796829008abf506ce113b3aedcf98cacceca
Guilherme Crocetti <guilherme.crocetti@secomind.com>
2024-11-19 17:36:50 +0100

    fix: invalid variable name
    Signed-off-by: Guilherme Crocetti <guilherme.crocetti@secomind.com>

--- modules/iam_service_account/variables.tf
+++ modules/iam_service_account/variables.tf
@@ -19,11 +19,11 @@ variable "name" {
 variable "display_name" {
   type        = string
   description = "The name to be displayed"
-  default     = "service account ${var.name}"
+  default     = "service account"
 }
 
 variable "description" {
   type        = string
   description = "description of the service account"
-  default     = "service account ${var.name}"
+  default     = "service account"
 }
