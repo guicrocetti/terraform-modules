1f8b37d28f60e26faae24d91053487c008cb40cd
Guilherme Crocetti <guilherme.crocetti@secomind.com>
2024-11-21 18:21:35 +0100

    chore(argocd): remove values from argocd
    Signed-off-by: Guilherme Crocetti <guilherme.crocetti@secomind.com>

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -14,10 +14,6 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.14.0"
     }
-    kubectl = {
-      source  = "alekc/kubectl"
-      version = "~> 2.0.4"
-    }
   }
 }
 
@@ -51,96 +47,18 @@ provider "helm" {
   }
 }
 
-provider "kubectl" {
-  host = "https://${var.cluster_endpoint}"
-  cluster_ca_certificate = try(
-    base64decode(var.cluster_ca_certificate),
-    null
-  )
-  token = data.google_client_config.provider.access_token
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "gke-gcloud-auth-plugin"
-  }
-}
-
 
 # ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY ARGOCD
 # ---------------------------------------------------------------------------------------------------------------------
 
-
-resource "kubectl_manifest" "argocd_namespace" {
-  yaml_body  = <<YAML
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: argocd
-YAML
-  apply_only = true
-}
-
 resource "helm_release" "argocd" {
-  chart      = "argo-cd"
-  name       = "argocd"
-  namespace  = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  version    = "v7.7.3"
-
-  values = [
-    yamlencode({
-      # domain = "${var.argo_domain}"
-
-      certificate = {
-        enabled = false
-      }
-
-      "argo-cd" = {
-        "redis-ha" = {
-          enabled = true
-        }
-
-        controller = {
-          replicas = 1
-        }
-
-        # server = {
-        #   extraArgs = ["--insecure"]
-        #   replicas  = 2
-        #   ingress = {
-        #     enabled          = true
-        #     ingressClassName = "nginx"
-        #     annotations = {
-        #       "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true"
-        #       "nginx.ingress.kubernetes.io/ssl-passthrough"    = "true"
-        #     }
-        #     tls = true
-        #     config = {
-        #       tls = {
-        #         secretName = "argocd-server-tls"
-        #       }
-        #     }
-        #   }
-        # }
-
-        repoServer = {
-          replicas = 2
-        }
-
-        applicationSet = {
-          replicas = 2
-        }
-
-        dex = {
-          enabled = true
-        }
-
-        notifications = {
-          enabled = true
-        }
-      }
-    })
-  ]
+  chart            = "argo-cd"
+  name             = "argocd"
+  namespace        = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  version          = "v7.7.3"
+  create_namespace = true
 
   dependency_update = true
   lifecycle {
@@ -148,28 +66,4 @@ resource "helm_release" "argocd" {
   }
 
   atomic = false
-
-  # depends_on = [
-  #   kubectl_manifest.argocd_namespace,
-  #   google_container_node_pool.general,
-  #   helm_release.external_secrets,
-  #   helm_release.ingress-nginx
-  # ]
 }
-
-# resource "kubectl_manifest" "AppProject" {
-#   yaml_body = templatefile("${path.module}/argocd/application.yaml", {
-#     repo_username = var.git_repository_username
-#     repo_name     = var.git_repository
-#     cluster_name  = var.cluster_name
-#   })
-
-#   depends_on = [helm_release.argocd]
-
-#   wait_for {
-#     field {
-#       key   = "status.sync.status"
-#       value = "Synced"
-#     }
-#   }
-# }
