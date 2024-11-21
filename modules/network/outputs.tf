db1ffd07afcbadb43f6a8989a4e64df5c6a3952f
Guilherme Crocetti <guilherme.crocetti@secomind.com>
2024-11-21 17:25:34 +0100

    fix(network): Ignore changes to secondary_ip_range in private subnetwork
    Signed-off-by: Guilherme Crocetti <guilherme.crocetti@secomind.com>

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,30 +1,29 @@
 output "network_self_link" {
   description = "VPC Self-link"
-  value       = data.google_compute_network.default[0].name != null ? data.google_compute_network.default[0].self_link : google_compute_network.private[0].self_link
+  value       = var.create_vpc ? google_compute_network.private[0].self_link : data.google_compute_network.default[0].self_link
 }
 
 output "subnetwork_self_link" {
   description = "subnetwork self link"
-  value       = data.google_compute_subnetwork.default[0].name != null ? data.google_compute_subnetwork.default[0].self_link : google_compute_subnetwork.private[0].self_link
+  value       = var.create_vpc ? google_compute_subnetwork.private[0].self_link : data.google_compute_subnetwork.default[0].self_link
 }
 
 output "network_name" {
   description = "VPC Name"
-  value       = data.google_compute_network.default[0].name != null ? data.google_compute_network.default[0].name : google_compute_network.private[0].name
+  value       = var.create_vpc ? google_compute_network.private[0].name : data.google_compute_network.default[0].name
 }
 
 output "ip_cidr_range" {
-  value = data.google_compute_subnetwork.default[0].name != null ? data.google_compute_subnetwork.default[0].ip_cidr_range : google_compute_subnetwork.private[0].ip_cidr_range
+  value = var.create_vpc ? google_compute_subnetwork.private[0].ip_cidr_range : data.google_compute_subnetwork.default[0].ip_cidr_range
 }
 
 output "network_cidr_ranges" {
   description = "List of CIDR Blocks"
   value = compact([
-    data.google_compute_subnetwork.default[0].name != null ?
+    !var.create_vpc ?
     data.google_compute_subnetwork.default[0].ip_cidr_range :
     google_compute_subnetwork.private[0].ip_cidr_range,
 
-    # Lógica similar para secondary ranges, se aplicável
     try(data.google_compute_subnetwork.default[0].secondary_ip_range[0].ip_cidr_range,
     google_compute_subnetwork.private[0].secondary_ip_range[0].ip_cidr_range),
 
