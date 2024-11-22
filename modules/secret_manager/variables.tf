313297d25e0704c4f6dc05d52397d651a281f3bd
Guilherme Crocetti <guilherme.crocetti@secomind.com>
2024-11-22 10:48:51 +0100

    chore(secret_manager): update variables description
    Signed-off-by: Guilherme Crocetti <guilherme.crocetti@secomind.com>

--- modules/secret_manager/variables.tf
+++ modules/secret_manager/variables.tf
@@ -1,14 +1,26 @@
-variable "secret_name" {
-  type    = string
-  default = "default_cluster_secret_data"
+variable "cluster_name" {
+  description = "output of module cluster -> cluster_name"
+  type        = string
 }
 
-variable "cluster_name" { type = string }
-variable "cluster_server" { type = string }
-variable "cluster_ca_data" { type = string }
+variable "cluster_server" {
+  description = "output of module cluster -> cluster_server"
+  type        = string
+}
+
+variable "cluster_ca_data" {
+  description = "output of module cluster -> cluster_ca_data"
+  type        = string
+}
+
+variable "secret_name" {
+  description = "Name of the secret to be stored in secret manager"
+  type        = string
+  default     = "default_cluster_secret_data"
+}
 
 variable "label" {
-  description = "secret manager label"
+  description = "optional secret manager label"
   type        = string
   default     = "cluster_secret_data"
 }
