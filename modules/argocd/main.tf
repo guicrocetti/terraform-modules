6117a6bdbd432e9b0641e17ddb600c39b54d1044
guicrocetti <crocetti@outlook.com>
2024-11-29 16:20:33 +0100

    update(argocd): apply argocd resources on setup
    Signed-off-by: guicrocetti <crocetti@outlook.com>

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.14.0"
     }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0.4"
+    }
   }
 }
 
@@ -47,6 +51,18 @@ provider "helm" {
   }
 }
 
+provider "kubectl" {
+  host = "https://${var.cluster_endpoint}"
+  cluster_ca_certificate = try(
+    base64decode(var.cluster_ca_certificate),
+    null
+  )
+  token = data.google_client_config.provider.access_token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "gke-gcloud-auth-plugin"
+  }
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY ARGOCD
@@ -61,9 +77,49 @@ resource "helm_release" "argocd" {
   create_namespace = true
 
   dependency_update = true
+
   lifecycle {
     ignore_changes = all
   }
 
   atomic = false
+}
+
+resource "kubernetes_secret" "argocd_repo_secret" {
+  depends_on = [helm_release.argocd]
+
+  metadata {
+    name      = "private-repo"
+    namespace = "argocd"
+    labels = {
+      "argocd.argoproj.io/secret-type" = "repository"
+    }
+  }
+
+  data = {
+    type     = "git"
+    url      = "https://github.com/${var.repository_name}.git"
+    username = var.repo_username
+    password = var.argocd_repo_token
+  }
+}
+
+resource "null_resource" "git_clone_and_apply" {
+  depends_on = [helm_release.argocd, kubernetes_secret.argocd_repo_secret]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      # Clone the private repository using the provided token
+      git clone https://${var.argocd_repo_token}@github.com/${var.repository_name}.git /tmp/argocd-config
+      
+      # Wait for ArgoCD to be ready
+      kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
+      
+      # Apply the kustomization
+      kubectl apply -k /tmp/argocd-config/argocd
+      
+      # Cleanup
+      rm -rf /tmp/argocd-config
+    EOT
+  }
 }
